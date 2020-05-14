48e52f5ff2cb00e90b390f594998fbbb02d2520c
None <None>
2020-05-14 10:43:48 +0800

    Finish Lab1 LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,19 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author alantes
  */
 public class LeapYear {
+    /** A leap year is either:
+     * divisible by 400 or
+     * divisible by 4 and not by 100. */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0){
+            return true;
+        }else if((year % 4 ==0) && (year % 100 != 0)){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
